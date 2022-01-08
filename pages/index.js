b5588304ad711abaa411a74caea1f90201e66114
Ori Mizrahi <omizrahi@Oris-MacBook-Air.local>
2022-01-08 00:00:33 -0800

    created questions

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
-
+import { questions } from "../questions.js";
 import config from "../chatbot/config.js";
 import MessageParser from "../chatbot/Messageparser.js";
 import ActionProvider from "../chatbot/ActionProvider.js";
